c3113a323db6c4e27f84d63ae214784f269e9567
DamiAkinniyi <oluwadamilola.akinniyi@students.iaac.net>
2022-06-29 07:25:30 -0500

    Added JS

---
+++ script.js
@@ -0,0 +1,51 @@
+// function checkDriverAge(age){
+//     console.log(age)
+
+//     if (Number(age) < 18) {
+//         console.log("Sorry, you are too yound to drive this car. Powering off");
+//     } else if (Number(age) > 18) {
+//         console.log("Powering On. Enjoy the ride!");
+//     } else if (Number(age) === 18) {
+//         console.log("Congratulations on your first year of driving. Enjoy the ride!");
+//     }
+// }
+
+// checkDriverAge(25)
+
+
+// var checkDriverAge2 = function(age){
+
+//     if (Number(age) < 18) {
+//         console.log("Sorry, you are too yound to drive this car. Powering off");
+//     } else if (Number(age) > 18) {
+//         console.log("Powering On. Enjoy the ride!");
+//     } else if (Number(age) === 18) {
+//         console.log("Congratulations on your first year of driving. Enjoy the ride!");
+//     }
+// }
+// checkDriverAge2(44)
+
+// {
+//     username:'dami', 
+//     password:'mypassword'
+// }
+// var database = [
+//     {
+//         username:'dami',
+//         password:'mypassword'
+//     }
+// ]
+// var newsfeed = [
+//     {
+//         username:"one", 
+//         timeline:"I have timeline one"
+//     },
+//     {
+//         username:"two", 
+//         timeline:"I have timeline two"
+//     },
+//     {
+//         username:"three", 
+//         timeline:"I have timeline three"
+//     }
+// ]
