3c3a36fac4c4170e9b274626ce51b3db3c1337b5
DamiAkinniyi <oluwadamilola.akinniyi@students.iaac.net>
2022-07-17 06:26:10 -0500

    changed landing page format

--- src/script.js
+++ src/script.js
@@ -2,6 +2,11 @@
 const nav = document.querySelector('#navbar');
 console.log(nav)
 
+
+//Landing page animation
+let mtn = document.querySelector('#mtn_txt')
+console.log(mtn)
+
 //Slideshow
 let slides = document.querySelectorAll('.slide');
 console.log(slides)
